98bde84f6d602854df36f013435ac9b4b1089df5
Marco Scholl <develop@marco-scholl.de>
2011-09-16 21:27:56 +0200

    better check

--- app/controllers/github/api_controller.rb
+++ app/controllers/github/api_controller.rb
@@ -2,11 +2,13 @@ class Github::ApiController < ApplicationController
   def hook
     payload = JSON.parse(params[:payload])
 
-    Github::Repository.find_or_create_by_url(
-      {
-        name: payload['repository']['name'],
-        url: payload['repository']['url']
-      }
-    )
+    unless payload['repository'].blank?
+      Github::Repository.find_or_create_by_url(
+        {
+          name: payload['repository']['name'],
+          url: payload['repository']['url']
+        }
+      )
+    end
   end
 end
