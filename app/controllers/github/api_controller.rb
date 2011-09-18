a7447e0cc89f4c1e48f917ffc13f8c999b1cfefc
Marco Scholl <develop@marco-scholl.de>
2011-09-18 21:50:54 +0200

    more debug

--- app/controllers/github/api_controller.rb
+++ app/controllers/github/api_controller.rb
@@ -1,30 +1,29 @@
 class Github::ApiController < ApplicationController
   def hook
     payload = JSON.parse(params[:payload])
+    Rails.logger.info(payload.inspect)
 
-    unless payload['repository'].blank?
-      repository = Repository.find_by_url(payload['repository']['url'])
-      
-      if repository.nil?
-        repository = Repository.create(
+    repository = Repository.find_by_url(payload['repository']['url'])
+    
+    if repository.nil?
+      repository = Repository.create(
+        {
+          :name => payload['repository']['name'],
+          :url => payload['repository']['url']
+        }
+      )
+    end
+
+    if repository.autobuild?
+      branch = payload['ref'].gsub("refs/heads/", '')
+      BuildMapping.where(:branch => branch).all.each do |build_mapping|
+        Build.create!(
           {
-            :name => payload['repository']['name'],
-            :url => payload['repository']['url']
+            :repository_id => repository.id,
+            :dist => build_mapping.dist,
+            :stage => build_mapping.stage
           }
         )
-      end
-
-      if repository.autobuild?
-        branch = payload['ref'].gsub("refs/heads/", '')
-        BuildMapping.where(:branch => branch).all.each do |build_mapping|
-          Build.create!(
-            {
-              :repository_id => repository.id,
-              :dist => build_mapping.dist,
-              :stage => build_mapping.stage
-            }
-          )
-        end
       end
 
       UserMailer.commit(payload).deliver
