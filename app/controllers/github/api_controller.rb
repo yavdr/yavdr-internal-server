8d65de519fa1c719a9c1a0ed01e058de74b59686
Marco Scholl <develop@marco-scholl.de>
2011-09-18 12:37:43 +0200

    update hook for github

--- app/controllers/github/api_controller.rb
+++ app/controllers/github/api_controller.rb
@@ -3,12 +3,24 @@ class Github::ApiController < ApplicationController
     payload = JSON.parse(params[:payload])
 
     unless payload['repository'].blank?
-      Github::Repository.find_or_create_by_url(
+      repository = ::Repository.find_or_create_by_url(
         {
           name: payload['repository']['name'],
           url: payload['repository']['url']
         }
       )
+
+      branch = payload['ref'].gsub("refs/heads/", '')
+
+      BuildMapping.where(:branch => branch).all.each do |build_mapping|
+        Build.create!(
+          {
+            :repository_id => repository.id,
+            :dist => build_mapping.dist,
+            :stage => build_mapping.stage
+          }
+        )
+      end
 
       UserMailer.commit(payload).deliver
 
