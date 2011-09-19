95084dd74685be090344180a2a9a667db1ec5c6c
Marco Scholl <develop@marco-scholl.de>
2011-09-19 09:22:17 +0200

    :without_protection users edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   load_and_authorize_resource
 
   def update
-    if @user.update_attributes params[:user]
+    if @user.update_attributes params[:user], :without_protection => true
       redirect_to :users, :notice => t('controller.users.saved')
     else
       render :edit
