48b64f3fc0cf0b8025244d320ed8f36b70f83595
Marco Scholl <develop@marco-scholl.de>
2011-09-21 09:13:56 +0200

    bug: disable assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ YavdrInternalServer::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  config.action_controller.asset_host = "http://htz04.phatserver.de/yavdr-internal-server/assets/"
+  #config.action_controller.asset_host = "http://htz04.phatserver.de/yavdr-internal-server/assets/"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
