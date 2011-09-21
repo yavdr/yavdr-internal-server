8a8496116e59337e5f73396dc27387394cac3081
Marco Scholl <develop@marco-scholl.de>
2011-09-21 20:25:18 +0200

    new style

--- script/convert_vdr1.sh
+++ script/convert_vdr1.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-convert $1 \( +clone -alpha extract -draw 'fill black polygon 0,0 0,15 15,0 fill white circle 15,15 15,0' \( +clone -flip \) -compose Multiply -composite \( +clone -flop \) -compose Multiply -composite \) -alpha off -compose CopyOpacity -composite $2
+
+convert $1 \( +clone -alpha extract -draw 'fill black polygon 0,0 0,20 20,0 fill white circle 20,20 20,0' \( +clone -flip \) -compose Multiply -composite \( +clone -flop \) -compose Multiply -composite \) -alpha off -compose CopyOpacity -composite $2
