7e784765ec6fe25e54f9c48f66a5d5853f6f7616
Marco Scholl <develop@marco-scholl.de>
2011-12-23 19:46:13 +0100

    add experimental to buildserver

--- config/initializers/lists.rb
+++ config/initializers/lists.rb
@@ -1,5 +1,5 @@
 YavdrInternalServer::DISTS = ['natty', 'oneiric', 'precise']
-YavdrInternalServer::STAGES = ['unstable', 'testing', 'stable']
+YavdrInternalServer::STAGES = ['unstable', 'experimental', 'testing', 'stable']
 YavdrInternalServer::BUILD_STATUS = ['waiting', 'inprocess', 'complete', 'error']
 YavdrInternalServer::ROLES = { 'logo' => 'Logoverwaltung', 'yavdr' => 'yaVDR' }
 YavdrInternalServer::SECTIONS = ['main', 'yavdr', 'vdr', 'xbmc']
